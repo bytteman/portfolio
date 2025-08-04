6861b8edefd5ddfd119b509de1ad02a496c0427d
devon <devon.y@gmail.com>
2025-08-03 22:14:34 -0400

    Connect to database

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    @profile = Profile.last
+    @works = Work.all.limit(100)
+    if @profile.nil?
+      render plain: "No profile found"
+    end
   end
 end
